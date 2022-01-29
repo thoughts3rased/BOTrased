803a9b88a19c30b1ab94211f52c8755b435447f5
thoughts3rased <STong212@gmail.com>
2022-01-29 12:22:08 +0000

    Changelog update finished, ready to merge into production

--- commands/changelog.js
+++ commands/changelog.js
@@ -12,37 +12,35 @@ module.exports = {
         var pages = []
         let embedBody
         let embedTitle
-        let j
-        fs.readdir('./changelogs/', (err, files)=> {
-                for (var i = 0; i < files.length; i++){
-                embedTitle = ''
-                embedBody = ''
-                const changelogFileStream = fs.createReadStream(`./changelogs/${files[i]}`);
-                const rl = readline.createInterface({
-                    input: changelogFileStream,
-                    crlfDelay: Infinity
-                });
-                j = 0
-                //this should split each file of the changelog onto its own line
-                rl.on('line', (line) => {
-                    if (j === 0){
-                        //we want the first line to be the title of the embed, with the rest being in the embed's description
-                        embedTitle = line
-                        console.log(embedTitle)
-                    } else{
-                        embedBody += line + "\n"
-                        console.log(embedBody + 1)
-                    };
-                    j++
-                })
-                //console.log(embedTitle, embedBody)
-                const embed = new MessageEmbed()
-                .setColor('DARK_PURPLE')
-                .setTitle(embedTitle)
-                .setDescription(embedBody)
-                ;
-                pages.push(embed)
-        }})
+        const filenames = fs.readdirSync('./changelogs').reverse();
+        console.log(filenames)   
+        for(var i = 0; i < filenames.length; i++){
+            embedTitle = ''
+            embedBody = ''
+            const changelogFileStream = fs.createReadStream(`./changelogs/${filenames[i]}`);
+            const rl = readline.createInterface({
+                input: changelogFileStream,
+                crlfDelay: Infinity
+            });
+            var j = 0
+            //this should split each file of the changelog onto its own line
+            for await(const line of rl){
+                if (j === 0){
+                    //we want the first line to be the title of the embed, with the rest being in the embed's description
+                    embedTitle = line
+                } else{
+                    embedBody += line + "\n"
+                };
+                j++
+            }
+            //console.log(embedTitle, embedBody)
+            var embed = new MessageEmbed()
+            .setColor('DARK_PURPLE')
+            .setTitle(embedTitle)
+            .setDescription(embedBody)
+            ;
+            pages.push(embed)
+        }
         
         //console.log(pages)
         const buttonList = [
