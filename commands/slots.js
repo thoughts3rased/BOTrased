7a2e385e9d1e7a86d0fd1f90fd93dcdb186ab0be
thoughts3rased <STong212@gmail.com>
2022-02-05 17:53:45 +0000

    Added slots command

---
+++ commands/slots.js
@@ -0,0 +1,58 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('slots')
+        .setDescription('Gamble away your credits in a text-based slot machine.')
+        .addIntegerOption(option =>
+            option
+                .setName("bet")
+                .setDescription("The amount you want to wager")
+                .setRequired(true)),
+    async execute(interaction) {
+        const user = await userRecords.findOne({where: {userID: interaction.user.id}});
+        if (user.get("money") < interaction.options.getInteger("bet")){
+            return await interaction.reply(":x: You don't have enough credits to bet that amount.")
+        } else if (interaction.options.getInteger("bet") < 0) {
+            return await interaction.reply(":x: You cannot bet an amount that is less than 0.")
+        }
+        await interaction.reply(":question: :question: :question:")
+        await userRecords.increment({money: 0 - interaction.options.getInteger("bet")}, {where: {userID: interaction.user.id}})
+        const fruitTable = fruits = [":watermelon:", ":grapes:", ":grapes:",  ":banana:", ":banana:", ":banana:",  ":pineapple:", ":pineapple:", ":pineapple:", ":pineapple:",  ":cherries:", ":cherries:", ":cherries:", ":cherries:", ":cherries:"]
+        const fruitRewardDict = {
+            ":watermelon:": 50,
+            ":grapes:": 15,
+            ":banana:": 8,
+            ":pineapple:": 6,
+            ":cherries:": 2
+        }
+        var result = []
+        for (var i = 0; i < 3; i++){
+            result.push(fruitTable[Math.floor(Math.random()*(fruitTable.length-1))])
+        }
+        console.log(result)
+        await interaction.editReply(`${result[0]} :question: :question:`)
+        setTimeout(function(){
+            interaction.editReply(`${result[0]} ${result[1]} :question:`)
+            setTimeout(function(){
+                interaction.editReply(`${result[0]} ${result[1]} ${result[2]}`).then(() =>{
+                    var payout = 0
+                    if (result[0] === result[1] && result[1] === result[2]){
+                        payout = interaction.options.getInteger("bet") * fruitRewardDict[result[0]]
+                        if (result[0] != ":watermelon:"){
+                             interaction.followUp(`Congratulations, you've won ${payout} credits!`)
+                        } else {
+                             interaction.followUp(`Hold on... Woah! Congratulations! You've hit the jackpot, meaning you've won 50 times the amount you bet! Enjoy your ${payout} credits!`)
+                        }
+                    } else if (result[0] === result[1]){
+                        payout = interaction.options.getInteger("bet")
+                         interaction.followUp("Not bad... Matching two fruits means that you've won your money back. You can always try again you know.")
+                    } else {
+                         interaction.followUp("Ah, no luck today I see. Better luck next time!")
+                    }
+                    userRecords.increment({money: payout}, {where: {userID: interaction.user.id}})
+                })
+            }, 500)
+        }, 1000)
+    },
+};
