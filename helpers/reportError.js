20c37e8c87e1995a34e30ebdd4d47b2a5bd39382
thoughts3rased <STong212@gmail.com>
2022-09-25 01:32:02 +0100

    Final part of V2.6

--- helpers/reportError.js
+++ helpers/reportError.js
@@ -1,6 +1,11 @@
-const Sequelize = require("sequelize")
-
-
+/**
+ * Writes an error report to the database
+ * @param {string} errorId The error's ID
+ * @param {string} errorStack The stack trace for the error
+ * @param {string} command The command in which the error occurred
+ * @param {string} commandAuthor The user who invoked the command
+ * @param {string} commandServer The server where the command was invoked
+ */
 async function reportError(errorId, errorStack, command = null, commandAuthor = null, commandServer = null){
     await global.errorTable.create({
         errorId: errorId,
